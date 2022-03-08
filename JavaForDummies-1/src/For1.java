83a052287bca5da8a81bcb875fd9e5a4aa05ddb9
Евгений <forprog2008@hotmail.com>
2022-03-08 20:02:11 +0400

    More For

--- JavaForDummies-1/src/For1.java
+++ JavaForDummies-1/src/For1.java
@@ -2,11 +2,11 @@ import static java.lang.System.out;
 
 
 public class For1 {
-public static void main(String args[]) {
-	
-	for (int i = 1; i <= 10; i++) {
-		out.println("Значение счетчика равно " + i + ".");
+	public static void main(String args[]) {
+
+		for (int i = 1; i <= 10; i++) {
+			out.println("Значение счетчика равно " + i + ".");
+		}
+		out.println("Достаточно!!!!");
 	}
-	out.println("Достаточно!!!!");
-}
 }
