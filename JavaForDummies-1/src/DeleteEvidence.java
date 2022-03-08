263b8f005033c0bdeffd83681d7c4640b6077bcb
Евгений <forprog2008@hotmail.com>
2022-03-08 20:45:21 +0400

    More For 3

---
+++ JavaForDummies-1/src/DeleteEvidence.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class DeleteEvidence {
+
+	public static void main(String args[]) {
+		File evidence = new File("cookedBooks.txt");
+		Scanner keyboard = new Scanner(System.in);
+		char reply;
+		
+		do {
+			out.print("Delete filed? (y/n)");
+			reply = keyboard.findWithinHorizon(".", 0).charAt(0);
+		} while (reply != 'y' && reply != 'n');
+		
+		if (reply == 'y') {
+			out.println("OK, delete");
+			evidence.delete();
+			out.println("File deleted");
+		} else {
+			out.println("OK, leaving");
+		}
+		
+		keyboard.close();
+	}
+}
