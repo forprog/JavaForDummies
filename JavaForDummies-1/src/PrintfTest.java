7ab6ebf1357e0dd6f42c057d0a4438f8eee606c9
Евгений <forprog2008@hotmail.com>
2022-03-13 18:27:37 +0400

    used locale

--- JavaForDummies-1/src/PrintfTest.java
+++ JavaForDummies-1/src/PrintfTest.java
@@ -11,7 +11,7 @@ public class PrintfTest {
 		out.printf("%s%+d%s", ">>",7,"<<\n");
 		out.printf("%s%+d%s", ">>",-7,"<<\n");
 		out.printf("%s%(d%s", ">>",-7,"<<\n");
-		out.printf("%s%10.5f%s", ">>",7.0,"<<\n");//Locale.US,
+		out.printf(Locale.US,"%s%10.5f%s", ">>",7.0,"<<\n");
 		out.printf("%s%S%s", ">>","Hello","<<\n");
 		out.printf("%s%s%s", ">>",'x',"<<\n");
 		out.printf("%s%S%s", ">>",'x',"<<\n");
