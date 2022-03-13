7e2e65dd333657be49d72bb7e6a7e37234bd0eb6
Евгений <forprog2008@hotmail.com>
2022-03-13 18:26:35 +0400

    learned prinf

---
+++ JavaForDummies-1/src/PrintfTest.java
@@ -0,0 +1,20 @@
+import static java.lang.System.out;
+
+import java.util.Locale;
+
+public class PrintfTest {
+	public static void main(String args[]) {
+		out.printf("%s%d%s", ">>",7,"<<\n");
+		out.printf("%s%8d%s", ">>",7,"<<\n");
+		out.printf("%s%-8d%s", ">>",7,"<<\n");
+		out.printf("%s%08d%s", ">>",7,"<<\n");
+		out.printf("%s%+d%s", ">>",7,"<<\n");
+		out.printf("%s%+d%s", ">>",-7,"<<\n");
+		out.printf("%s%(d%s", ">>",-7,"<<\n");
+		out.printf("%s%10.5f%s", ">>",7.0,"<<\n");//Locale.US,
+		out.printf("%s%S%s", ">>","Hello","<<\n");
+		out.printf("%s%s%s", ">>",'x',"<<\n");
+		out.printf("%s%S%s", ">>",'x',"<<\n");
+	}
+}
+
