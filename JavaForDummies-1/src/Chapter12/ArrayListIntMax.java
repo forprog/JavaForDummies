4adfd14c59231e6084254f6bddc59046cad1d871
forpr <forpr@DESKTOP-QSTDPGK>
2022-10-23 13:57:16 +0400

    Added Chapter 12

---
+++ JavaForDummies-1/src/Chapter12/ArrayListIntMax.java
@@ -0,0 +1,26 @@
+package Chapter12;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListIntMax {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> arList = new ArrayList<>();
+		arList.add(85);
+		arList.add(19);
+		arList.add(0);
+		arList.add(103);
+		arList.add(13);
+
+		Integer max = Integer.MIN_VALUE;
+		for (Integer curVal : arList) {
+			if (max < curVal) {
+				max = curVal;
+			}
+		}
+		
+		System.out.println(max);
+	}
+
+}
