e44bd87e9b8ea4e9ebb8aeede106aa4a90a98cb1
forpr <forpr@DESKTOP-QSTDPGK>
2022-10-25 09:06:32 +0400

    added TallySales

---
+++ JavaForDummies-1/src/Chapter12/Sale.java
@@ -0,0 +1,24 @@
+package Chapter12;
+
+public class Sale {
+	private String item;
+	private double price;
+	
+	public Sale(String item, double price) {
+		super();
+		this.item = item;
+		this.price = price;
+	}
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
