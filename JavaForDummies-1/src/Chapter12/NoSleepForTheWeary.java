6f145c94f94fea9e5b20f515005f93d5696dff4c
forpr <forpr@DESKTOP-QSTDPGK>
2022-11-06 10:38:34 +0400

    NoSleep

---
+++ JavaForDummies-1/src/Chapter12/NoSleepForTheWeary.java
@@ -0,0 +1,15 @@
+package Chapter12;
+
+import static java.lang.System.out;
+
+public class NoSleepForTheWeary {
+	public static void main(String args[]) throws InterruptedException {
+		out.println("Taking nap");
+		takeANap();
+	}
+
+	private static void takeANap() throws InterruptedException {
+		Thread.sleep(5000);
+		
+	}
+}
